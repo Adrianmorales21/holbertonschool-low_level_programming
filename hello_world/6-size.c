1f591dbbd87b3fd33ebaa3bd2214ce1fe1242248
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-02-17 21:11:55 +0000

    git

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,14 +6,10 @@
 
 int main(void)
 {
-int intType;
-float floatType;
-double doubleType;
-char charType;
 
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of double: %zu bytes\n", sizeof(doubleType));
-printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of a char: %d bytes\n", sizeof(char));
+printf("Size of an int: %d bytes\n", sizeof(int));
+printf("Size of a long int: %d bytes\n", sizeof(long int));
+printf("Size of a float: %d byte\n", sizeof(float));
 return (0);
 }
