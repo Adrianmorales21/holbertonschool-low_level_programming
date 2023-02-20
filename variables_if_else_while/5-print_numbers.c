82356ef1e53f42d929fadad3fc054bc3fbd65a40
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-02-20 10:01:42 +0000

    git push

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- numbers 0 10
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 < 10; n++;)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
