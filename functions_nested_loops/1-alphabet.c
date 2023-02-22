11ffb01f118b382bac82689b81d74cbfbbe0f2be
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-02-22 23:06:06 +0000

    git

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main -print alpha
+ *Return:void
+ */
+void printf_alphabet(void)
+{
+       int a;
+	
+	for (a = 97; a <= 122; a++)
+	{
+
+	_putchar(a);
+	}
+	_putchar('\n');
+}
