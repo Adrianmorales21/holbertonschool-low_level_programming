d410bf527d413f898d95266f4c306d39fbef81bb
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-02-23 20:37:43 +0000

    git push

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add- add 2 integers
+ * @a: add to b
+ * @b:add to a
+ * Return: te value of the sum
+ */
+int add(int a, int b)u;
+{
+	int c;
+	 c = a + b;
+	 
+	 return (c);
+}
