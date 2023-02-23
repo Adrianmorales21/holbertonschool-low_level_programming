83dc85d238a15037f1a0d31c1ca807ab91f828fd
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-02-23 01:06:40 +0000

    git add

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main-alphabet on lower case X 10
+ * 
+ * Return: always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
