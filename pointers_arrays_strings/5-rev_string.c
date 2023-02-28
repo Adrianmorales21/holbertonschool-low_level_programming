e362b46a4800f611a1938d963d013f01b4d4707d
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-02-28 17:23:32 +0000

    new files

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string -reverse a string
+ * @s- string to reverse
+ */
+void rev_string(char *s)
+{
+	int len; 
+        int i = len;
+	char temp;
+
+	 for (len = 0;  *s != '\0'; l++, s++)
+
+
+        for (i - 1; i >= len / 2; i--);
+	{
+		temp = i;
+
+}
