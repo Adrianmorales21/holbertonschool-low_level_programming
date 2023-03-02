e6dd92059d558ae49fe1abd7047d8241c9a4d9f4
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-03-02 00:02:07 -0400

    git push

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,28 @@
 #include "main.h"
 /**
- * _strlen- lenght of string
- * @s:string
- * Return: string lenght size
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
  */
-int _strlen(char *s)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int l;
+	int j;
 
-	for (l = 0;  *s != '\0'; l++, s++)
-		;
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
 
-	return (l);
+	return (dest);
 }
+
---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strncpy-copy string.
+ * @dest:input
+ * @src:input
+ * @n:input
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n);
+{
+	int d, s;
+
+	d = 0;
+
+	while (dest[d] != '\0')
+
