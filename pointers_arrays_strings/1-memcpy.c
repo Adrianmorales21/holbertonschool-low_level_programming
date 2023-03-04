af0f82164a13026aef994b8017ba791be28c0f01
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-03-04 17:25:38 -0400

    memcopy function

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
- *
- *
- * ?
+ *_memcpy- cpies memory
+ *@dest:input
+ *@src:input
+ *@n:input
+ *Return:always dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
 
 	for (a = 0; a < n; a++)
-		dest[a] = *src;
+		dest[a] = src[a];
 	return (dest);
 }
