68dc1bee26263dd7b950b41547278728a03d9149
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-03-04 17:52:17 -0400

    _strchr function

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,17 +2,18 @@
 /**
  * _strchr - locates string
  * @s: string
- * @c: character to locate\
- *Return: s or null
+ * @c: character to locate
+ * Return: Always 0
  */
 char *_strchr(char *s, char c)
 {
 	int a;
 
-	for (a = 0; s[a] != '\0'; a++)
-	{
-		if (s[a] == c)
-			 return (s + a);
-	}
+for (a = 0; s[a] >= '\0'; a++)
+{
+	if (s[a] == c)
+
+		return (s + a);
+}
 	return ('\0');
 }
