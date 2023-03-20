797e17ed491adf27779723844742eed70efccddc
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-03-20 00:51:37 -0400

    git push

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog- structure for dog
+ * @name: name of the dog
+ * @age:age of the dog
+ * @owner:name of the owner
+ */
 struct dog
 {
 	char *name;
 	double age;
 	char *owner;
 };
+#endif
