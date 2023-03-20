f269531940cc396015048e85f7fb8a041e29cf82
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-03-20 17:17:42 -0400

    git push

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/*
+ * init_dog- initializes the doc struct
+ * @d: pointer to in initialize
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
