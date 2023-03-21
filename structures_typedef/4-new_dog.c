ba02fa4aa0171f35df2325e1a8024565f0d2c659
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-03-21 14:36:33 +0000

    git push

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <string.h>
+/**
+ * new_dog- creates new dog
+ *@name:name to copy
+ *@age:age to copy
+ *@owner:owner to copy
+ *Return: pointer to new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *nd;
+
+	nd = malloc(sizeof(dog_t));
+	nd->name = malloc(strlen(name + 1));
+	nd->owner = malloc(strlen(owner+1));
+	if (nd == NULL)
+	{
+		free(nd);
+		return (NULL);
+	}
+
+	nd->name = strdup(name);
+	nd->owner = strdup(owner);
+	nd->age = age;
+	
+	return (nd);
+}
