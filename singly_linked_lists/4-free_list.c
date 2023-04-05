77a0df83e6b159aa7a5b94d5a79b0e1bdf5e9803
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-04-04 22:45:07 -0400

    free a linked list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+4-free_list.c#include "lists.h"
+
+/**
+ * free_list- free linked list
+ * @head: linked list
+ */
+
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+	}
+}
