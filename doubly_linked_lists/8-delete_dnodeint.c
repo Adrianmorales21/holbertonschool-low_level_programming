075ccab62f1f1b20b6c9734f993892238344e3a1
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-04-06 23:36:33 -0400

    double link functions

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,41 +1,40 @@
 #include "lists.h"
-
 /**
- * delete_nodeint_at_index - deletes a node in a linked list at a certain index
- * @head: pointer to the first element in the list
- * @index: index of the node to delete
- *
- * Return: 1 (Success), or -1 (Fail)
+ * delete_dnodeint_at_index - deletes node at given idx
+ * @head: pointer to head of doubly linked list
+ * @index: index
+ * Return: 1 if successful, -1 if failed
  */
-int delete_nodeint_at_index(dlistint_t **head, unsigned int index)
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	listint_t *temp = *head;
-	listint_t *current = NULL;
-	unsigned int i = 0;
+	dlistint_t *del = NULL;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
+
+	del = *head;
 
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		free(temp);
+		free(del);
+		if (*head != NULL)
+			(*head)->prev = NULL;
 		return (1);
 	}
 
-	while (i < index - 1)
+	while ((index != 0) && (del->next != NULL))
 	{
-		if (!temp || !(temp->next))
-			return (-1);
-		temp = temp->next;
-		i++;
+		index -= 1;
+		del = del->next;
 	}
-
-
-	current = temp->next;
-	temp->next = current->next;
-	free(current);
-
-	return (1);
+	if (index == 0)
+	{
+		del->prev->next = del->next;
+		if (del->next != NULL)
+			del->next->prev = del->prev;
+		free(del);
+		return (1);
+	}
+	return (-1);
 }
-
